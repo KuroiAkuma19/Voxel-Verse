34086452b20cd85f4c6591b0b8e2666921e1a17d
Arnold Fernandes <arnold@Ghost.local>
2026-04-03 22:00:36 +0530

    Fix paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Voxel-Verse/',
   plugins: [react()],
-  base: "/Voxel-Verse/",
-});
+})
